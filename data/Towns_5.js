e66e1e3e9bfbd276303ac113df6c7aa386beeb7d
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-09 12:40:01 +0530

    Add files via upload

---
+++ data/Towns_5.js
@@ -0,0 +1,15 @@
+var json_Towns_5 = {
+"type": "FeatureCollection",
+"name": "Towns_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Ramanathapuram" }, "geometry": { "type": "Point", "coordinates": [ 78.844333752003223, 9.363301303728122, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Keelakarai" }, "geometry": { "type": "Point", "coordinates": [ 78.783973586314431, 9.232142126370846, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Devipattinam" }, "geometry": { "type": "Point", "coordinates": [ 78.893860224958658, 9.47339652076311, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Paramakudi" }, "geometry": { "type": "Point", "coordinates": [ 78.591863130983427, 9.544897182711571, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Muthukulathur" }, "geometry": { "type": "Point", "coordinates": [ 78.515452929103716, 9.350629652938293, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thondi" }, "geometry": { "type": "Point", "coordinates": [ 79.017357844726476, 9.739050588640563, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Sayalgudi" }, "geometry": { "type": "Point", "coordinates": [ 78.442708037642205, 9.169846442982362, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kamuthi" }, "geometry": { "type": "Point", "coordinates": [ 78.367064017698141, 9.410325369943541, -999999.0 ] } }
+]
+}
---
+++ data/VillageWiseFloodVulnerablity_3.js
@@ -0,0 +1,8 @@
+var json_VillageWiseFloodVulnerablity_3 = {
+"type": "FeatureCollection",
+"name": "VillageWiseFloodVulnerablity_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 78.644675255014647, 9.517881719633962 ] } }
+]
+}
